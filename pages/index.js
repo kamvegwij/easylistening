aadad854c9d2c996b99b698050d39fa971c230a2
Kamve Gwijana <gwijgwij011@gmail.com>
2022-11-30 22:26:26 +0200

    update UI

--- pages/index.js
+++ pages/index.js
@@ -8,26 +8,28 @@ export default function Home() {
       <Head>
         <title>Easy Listening</title>
       </Head>
-      <div className='topbar'>
-        <a href="profile"><li>Profile</li></a>
-        <a href="menu"><li>Menu</li></a>
-        <a href="help"><li>Help</li></a>
-        <a href="discover"><li>Discover</li></a>
-      </div>
+      <main>
+        <div className='topbar'>
+          <a href="profile"><li>Profile</li></a>
+          <a href="menu"><li>Menu</li></a>
+          <a href="help"><li>Help</li></a>
+          <a href="discover"><li>Discover</li></a>
+        </div>
 
-      <div className='maincontent'>
-        <div id="mainhead">
-          <h1>Easy Listening</h1>
+        <div className='maincontent'>
+          <div id="mainhead">
+            <h1>Easy Listening</h1>
+            </div>
+          
+          <div id="musiclist">
+            <h1>Songs for the day!</h1>
           </div>
-        
-        <div id="musiclist">
-          <h1>Songs for the day!</h1>
-        </div>
 
-        <div id="playmusic">
-          <h1>Start Listening</h1>
+          <div id="playmusic">
+            <h1>Start Listening</h1>
+          </div>
         </div>
-      </div>
+      </main>
     </div>
   )
 }
